1a428baa9d6bbe6b3c7b088120791c67a0d540e7
Christo De Lange <christo_de_lange@homedepot.com>
2016-09-27 19:37:28 -0400

    Adhere to gofmt -d -s .

--- cobra/cmd/root.go
+++ cobra/cmd/root.go
@@ -64,7 +64,7 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	}
 
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
