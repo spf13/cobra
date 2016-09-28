59e2709e24e367307c5e5603f727ffb74a24ebf6
DLDInternet <github@dldinternet.com>
2016-09-27 20:13:54 -0400

    Generated apps will need to inherit this fix

--- cobra/cmd/init.go
+++ cobra/cmd/init.go
@@ -205,13 +205,13 @@ func init() {
 {{ if .viper }}
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
+	viper.SetConfigName(".{{ .appName }}") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")  // adding home directory as first search path
+	if cfgFile != "" {            // enable ability to specify config file via flag
 		viper.SetConfigFile(cfgFile)
 	}
 
-	viper.SetConfigName(".{{ .appName }}") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
