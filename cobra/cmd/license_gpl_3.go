28fe9aaa166dfccc3881e4358d89c6534720326d
Albert Nigmatzianov <albertnigma@gmail.com>
2017-04-20 17:49:56 +0200

    cmd: Fix GPL 3 License header
    See #311

--- cobra/cmd/license_gpl_3.go
+++ cobra/cmd/license_gpl_3.go
@@ -23,16 +23,16 @@ func initGpl3() {
 This file is part of {{ .appName }}.
 
 {{ .appName }} is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
+it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 {{ .appName }} is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
+You should have received a copy of the GNU General Public License
 along with {{ .appName }}. If not, see <http://www.gnu.org/licenses/>.`,
 		Text: `                    GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
