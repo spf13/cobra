7b1b6e8dc027253d45fc029bc269d1c019f83a34
Albert Nigmatzianov <albertnigma@gmail.com>
2017-04-27 18:54:08 +0200

    cmd: Edit license texts

--- cobra/cmd/license_agpl.go
+++ cobra/cmd/license_agpl.go
@@ -678,6 +678,7 @@ specific requirements.
   You should also get your employer (if you work as a programmer) or school,
 if any, to sign a "copyright disclaimer" for the program, if necessary.
 For more information on this, and how to apply and follow the GNU AGPL, see
-<http://www.gnu.org/licenses/>.`,
+<http://www.gnu.org/licenses/>.
+`,
 	}
 }
