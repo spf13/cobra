e2f2121008461a75d2dc2a501616d83c1da78dd6
Albert Nigmatzianov <albertnigma@gmail.com>
2017-04-27 18:53:43 +0200

    cmd: Use 'this program' instead of appName in licenses

--- cobra/cmd/license_agpl.go
+++ cobra/cmd/license_agpl.go
@@ -4,21 +4,20 @@ func initAgpl() {
 	Licenses["agpl"] = License{
 		Name:            "GNU Affero General Public License",
 		PossibleMatches: []string{"agpl", "affero gpl", "gnu agpl"},
-		Header: `
-This file is part of {{ .appName }}.
+		Header: `{{.copyright}}
 
-{{ .appName }} is free software: you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-{{ .appName }} is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with {{ .appName }}. If not, see <http://www.gnu.org/licenses/>.`,
+along with this program. If not, see <http://www.gnu.org/licenses/>.`,
 		Text: `                    GNU AFFERO GENERAL PUBLIC LICENSE
                        Version 3, 19 November 2007
 
--- cobra/cmd/license_gpl_3.go
+++ cobra/cmd/license_gpl_3.go
@@ -19,21 +19,20 @@ func initGpl3() {
 	Licenses["gpl3"] = License{
 		Name:            "GNU General Public License 3.0",
 		PossibleMatches: []string{"gpl3", "gplv3", "gpl", "gnu gpl3", "gnu gpl"},
-		Header: `
-This file is part of {{ .appName }}.
+		Header: `{{.copyright}}
 
-{{ .appName }} is free software: you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-{{ .appName }} is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with {{ .appName }}. If not, see <http://www.gnu.org/licenses/>.`,
+along with this program. If not, see <http://www.gnu.org/licenses/>.`,
 		Text: `                    GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
 
