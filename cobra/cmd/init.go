9ef1cd9a2b5f89e91ca2909cb36fb1341fc91609
Albert Nigmatzianov <albertnigma@gmail.com>
2017-05-04 22:28:09 +0200

    cmd: Make detection of $HOME work on all systems
    Use github.com/mitchellh/go-homedir
    
    Fix #430

--- cobra/cmd/init.go
+++ cobra/cmd/init.go
@@ -23,10 +23,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-func init() {
-	RootCmd.AddCommand(initCmd)
-}
-
 var initCmd = &cobra.Command{
 	Use:     "init [name]",
 	Aliases: []string{"initialize", "initialise", "create"},
@@ -132,6 +128,7 @@ import (
 	"fmt"
 	"os"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 {{if .viper}}	"github.com/spf13/viper"{{end}}
 )
@@ -158,7 +155,7 @@ to quickly create a Cobra application.` + "`" + `,
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(-1)
+		os.Exit(1)
 	}
 }
 
@@ -166,7 +163,7 @@ func init() {
 {{if .viper}}	cobra.OnInitialize(initConfig){{end}}
 
 	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
+	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.{{ if .viper }}
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.{{ .appName }}.yaml)"){{ else }}
 	// RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.{{ .appName }}.yaml)"){{ end }}
@@ -178,14 +175,23 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
+	if cfgFile != "" {
+		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		viper.SetConfigName(".{{ .appName }}") // name of config file (without extension)
-		viper.AddConfigPath(os.Getenv("HOME")) // adding home directory as first search path
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(home)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cobra")
 	}
 
-	viper.AutomaticEnv()                   // read in environment variables that match
+	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
