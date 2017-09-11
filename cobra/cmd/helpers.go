eb14b2049f735e07f7256865c9333e9af45d3c49
Elliott Beach <elliott2.71828@gmail.com>
2017-09-11 10:13:31 -0500

    add support for go1.8 gopath

--- cobra/cmd/helpers.go
+++ cobra/cmd/helpers.go
@@ -21,6 +21,7 @@ import (
 	"path/filepath"
 	"strings"
 	"text/template"
+	"os/exec"
 )
 
 var cmdDirs = [...]string{"cmd", "cmds", "command", "commands"}
@@ -31,7 +32,27 @@ func init() {
 	envGoPath := os.Getenv("GOPATH")
 	goPaths := filepath.SplitList(envGoPath)
 	if len(goPaths) == 0 {
-		er("$GOPATH is not set")
+		// Adapted from https://github.com/Masterminds/glide/pull/798/files.
+		// As of Go 1.8 the GOPATH is no longer required to be set. Instead there
+		// is a default value. If there is no GOPATH check for the default value.
+		// Note, checking the GOPATH first to avoid invoking the go toolchain if
+		// possible.
+
+		goExecutable := os.Getenv("COBRA_GO_EXECUTABLE")
+		if len(goExecutable) <= 0 {
+			goExecutable = "go"
+		}
+
+		out, err := exec.Command(goExecutable, "env", "GOPATH").Output()
+		if err != nil {
+			er(err)
+		}
+
+		toolchainGoPath := strings.TrimSpace(string(out))
+		goPaths = filepath.SplitList(toolchainGoPath)
+		if len(goPaths) == 0 {
+			er("$GOPATH is not set")
+		}
 	}
 	srcPaths = make([]string, 0, len(goPaths))
 	for _, goPath := range goPaths {
