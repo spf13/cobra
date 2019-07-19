609f2c86328d24390019545f7a1a7666307ca679
Christian Muehlhaeuser <muesli@gmail.com>
2019-07-19 06:07:33 +0200

    Used goimports to fix import order
     Keeps the list sorted and prevents future merge conflicts.

--- cobra/cmd/project.go
+++ cobra/cmd/project.go
@@ -2,9 +2,10 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra/cobra/tpl"
 	"os"
 	"text/template"
+
+	"github.com/spf13/cobra/cobra/tpl"
 )
 
 // Project contains name, license and paths to projects.
