f5ce8029462b7cdc899b6c53b1a1a5c95c1c7efb
Dong Gang <dong.gang@daocloud.io>
2020-07-13 11:34:19 +0800

    Modify the logic of Command execute function
    Signed-off-by: Dong Gang <dong.gang@daocloud.io>

--- command.go
+++ command.go
@@ -801,12 +801,6 @@ func (c *Command) execute(a []string) (err error) {
 		}
 	}
 
-	if !c.Runnable() {
-		return flag.ErrHelp
-	}
-
-	c.preRun()
-
 	argWoFlags := c.Flags().Args()
 	if c.DisableFlagParsing {
 		argWoFlags = a
@@ -815,6 +809,12 @@ func (c *Command) execute(a []string) (err error) {
 	if err := c.ValidateArgs(argWoFlags); err != nil {
 		return err
 	}
+
+	if !c.Runnable() {
+		return flag.ErrHelp
+	}
+
+	c.preRun()
 
 	for p := c; p != nil; p = p.Parent() {
 		if p.PersistentPreRunE != nil {
