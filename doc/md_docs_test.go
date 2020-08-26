205791486a0069da0122575125dcfa3564ee6b69
Marc Lopez <marc5.12@outlook.com>
2020-08-26 16:42:38 +0300

    doc: GenMarkdown skip Synopsis on empty long cmd
    This patch modifies the GenMarkdownCustom to skip writing a Synopsis
    header with the `cmd.Short` duplicated both before and after the header.
    
    Instead, it only writes the `### Synopsis` header and the paragraph when
    a `cmd.Long` has some kind of content in it.
    
    Adds `TestGenMdDocWithNoLongOrSynopsis` as the test case.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- doc/md_docs_test.go
+++ doc/md_docs_test.go
@@ -28,6 +28,20 @@ func TestGenMdDoc(t *testing.T) {
 	checkStringContains(t, output, "Options inherited from parent commands")
 }
 
+func TestGenMdDocWithNoLongOrSynopsis(t *testing.T) {
+	// We generate on subcommand so we have both subcommands and parents.
+	buf := new(bytes.Buffer)
+	if err := GenMarkdown(dummyCmd, buf); err != nil {
+		t.Fatal(err)
+	}
+	output := buf.String()
+
+	checkStringContains(t, output, dummyCmd.Example)
+	checkStringContains(t, output, dummyCmd.Short)
+	checkStringContains(t, output, "Options inherited from parent commands")
+	checkStringOmits(t, output, "### Synopsis")
+}
+
 func TestGenMdNoHiddenParents(t *testing.T) {
 	// We generate on subcommand so we have both subcommands and parents.
 	for _, name := range []string{"rootflag", "strtwo"} {
