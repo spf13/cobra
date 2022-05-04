spf13/cobra

commit fb8031162c2ffab270774f13c6904bb04cbba5a7

perf(bash-v2): speed up filtering entries with descriptions (#1689)
Use simple prefix match instead of single word `compgen -W` command
substitution for each candidate match.

bash_completionsV2.go
@@ -191,8 +191,7 @@ __%[1]s_handle_standard_completion_case() {
         # Strip any description before checking the length
         comp=${compline%%%%$tab*}
         # Only consider the completions that match
-        comp=$(compgen -W "$comp" -- "$cur")
-        [[ -z $comp ]] && continue
+        [[ $comp == "$cur"* ]] || continue
         COMPREPLY+=("$compline")
         if ((${#comp}>longest)); then
             longest=${#comp}

commit 3e64861ebf343874ac0d89167e477d012f3006b3

fix(bash-v2): skip empty completions when filtering descriptions
`read` gives a last null value following a trailing newline.

Regression from fb8031162c2ffab270774f13c6904bb04cbba5a7.

bash_completionsV2.go
@@ -188,6 +188,7 @@ __%[1]s_handle_standard_completion_case() {
     local compline
     # Look for the longest completion so that we can format things nicely
     while IFS='' read -r compline; do
+        [[ -z $compline ]] && continue
         # Strip any description before checking the length
         comp=${compline%%%%$tab*}
         # Only consider the completions that match