ba5d4e28707c765c3ee12f55f0c2bf18512a6931
Toni Kangas <toni.kangas@upcloud.com>
2022-10-06 21:41:59 +0300

    Improve accessibility of markdown docs generation result
    Present available options and commands in tables. Also adds aliases of a command to markdown docs and omits short command description if long description is defined (similarly than in command usage template used for --help output).

--- doc/md_docs_test.go
+++ doc/md_docs_test.go
@@ -39,7 +39,7 @@ func TestGenMdDoc(t *testing.T) {
 	checkStringContains(t, output, rootCmd.Short)
 	checkStringContains(t, output, echoSubCmd.Short)
 	checkStringOmits(t, output, deprecatedCmd.Short)
-	checkStringContains(t, output, "Options inherited from parent commands")
+	checkStringContains(t, output, "Global options")
 }
 
 func TestGenMdDocWithNoLongOrSynopsis(t *testing.T) {
@@ -52,7 +52,7 @@ func TestGenMdDocWithNoLongOrSynopsis(t *testing.T) {
 
 	checkStringContains(t, output, dummyCmd.Example)
 	checkStringContains(t, output, dummyCmd.Short)
-	checkStringContains(t, output, "Options inherited from parent commands")
+	checkStringContains(t, output, "Global options")
 	checkStringOmits(t, output, "### Synopsis")
 }
 
@@ -76,7 +76,7 @@ func TestGenMdNoHiddenParents(t *testing.T) {
 	checkStringContains(t, output, rootCmd.Short)
 	checkStringContains(t, output, echoSubCmd.Short)
 	checkStringOmits(t, output, deprecatedCmd.Short)
-	checkStringOmits(t, output, "Options inherited from parent commands")
+	checkStringOmits(t, output, "Global options")
 }
 
 func TestGenMdNoTag(t *testing.T) {
