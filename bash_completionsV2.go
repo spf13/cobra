d115eb62494a45289f88d104d9605a6b127b9077
Gyanendra Mishra <anomaly.the@gmail.com>
2023-02-13 10:16:58 +0000

    support bash3

--- bash_completionsV2.go
+++ bash_completionsV2.go
@@ -363,7 +363,7 @@ __start_%[1]s()
 if [[ $(type -t compopt) = "builtin" ]]; then
     complete -o default -F __start_%[1]s %[1]s
 else
-    complete -F __start_%[1]s %[1]s
+    complete -o default -o nospace -F __start_%[1]s %[1]s
 fi
 
 # ex: ts=4 sw=4 et filetype=sh
