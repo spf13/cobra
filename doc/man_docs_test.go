60d056d1574d04c859638354bc9cacab70bab296
gocurr <xigua67damn@gmail.com>
2023-07-20 23:27:44 +0800

    doc: fix typo, Deperecated -> Deprecated (#2000)

--- doc/man_docs_test.go
+++ doc/man_docs_test.go
@@ -150,7 +150,7 @@ func TestGenManSeeAlso(t *testing.T) {
 	}
 }
 
-func TestManPrintFlagsHidesShortDeperecated(t *testing.T) {
+func TestManPrintFlagsHidesShortDeprecated(t *testing.T) {
 	c := &cobra.Command{}
 	c.Flags().StringP("foo", "f", "default", "Foo flag")
 	assertNoErr(t, c.Flags().MarkShorthandDeprecated("foo", "don't use it no more"))
