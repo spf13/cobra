187900ae5bc888d84ac95e94fc7f16b601cdc66f
Nir Soffer <nsoffer@redhat.com>
2024-08-24 06:52:56 +0300

    Replace deprecated ioutil usage
    Fixing golangci-lint errors[1]:
    
        Error: SA1019: "io/ioutil" has been deprecated since Go 1.19: As of
        Go 1.16, the same functionality is now provided by package [io] or
        package [os], and those implementations should be preferred in new
        code. See the specific function documentation for details.
        (staticcheck)
    
    [1] https://github.com/spf13/cobra/actions/runs/10535452454/job/29194442289?pr=2180

--- doc/man_docs_test.go
+++ doc/man_docs_test.go
@@ -18,7 +18,6 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -168,7 +167,7 @@ func TestManPrintFlagsHidesShortDeprecated(t *testing.T) {
 func TestGenManTree(t *testing.T) {
 	c := &cobra.Command{Use: "do [OPTIONS] arg1 arg2"}
 	header := &GenManHeader{Section: "2"}
-	tmpdir, err := ioutil.TempDir("", "test-gen-man-tree")
+	tmpdir, err := os.MkdirTemp("", "test-gen-man-tree")
 	if err != nil {
 		t.Fatalf("Failed to create tmpdir: %s", err.Error())
 	}
@@ -219,7 +218,7 @@ func assertNextLineEquals(scanner *bufio.Scanner, expectedLine string) error {
 }
 
 func BenchmarkGenManToFile(b *testing.B) {
-	file, err := ioutil.TempFile("", "")
+	file, err := os.CreateTemp("", "")
 	if err != nil {
 		b.Fatal(err)
 	}
